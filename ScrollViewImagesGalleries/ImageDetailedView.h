c94c58c4c0562e909b5c5a02580952b26c064c6b
nalhoussari <nalhoussari@hotmail.com>
2017-06-12 19:39:35 -0700

    Submitting the assignemnt after completing Part3

---
+++ ScrollViewImagesGalleries/ImageDetailedView.h
@@ -0,0 +1,13 @@
+//
+//  ImageDetailedView.h
+//  ScrollViewImagesGalleries
+//
+//  Created by Noor Alhoussari on 2017-06-12.
+//  Copyright © 2017 Noor Alhoussari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageDetailedView : UIViewController
+
+@end
